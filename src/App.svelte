6436b3c8bf80d74b79a1ce40cc8dee813052f55d
Dre Cadeddu <cadedduan@gmail.com>
2021-10-08 09:31:38 -0500

    first pass at cal

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	let calw = 7;
+	import Cal from './Cal.svelte';
+</script>
+
+<main>
+	<label>
+		<input type=number bind:value={calw} min=1 max=31>
+		<input type=range bind:value={calw} min=1 max=31>
+	</label>
+	<Cal start='2021-01-01' end='2022-01-01' w={calw}></Cal>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Cal.svelte
@@ -0,0 +1,44 @@
+<!-- let dates = Array.from('x'.repeat(365)).map(x=>new Date()); -->
+<script>
+    import dayjs from 'dayjs';
+    export let start='2021-01-01',end='2022-01-01', w=7;
+    const s = dayjs(start);
+    const e = dayjs(end);
+    let daysCount =  e.diff(s,'day');
+    let weeksCount = e.diff(s,'week');
+    let monthsCount = e.diff(s,'month');
+
+    let days = 'x'.repeat(daysCount).split('').map((x,i)=>s.add(i,'days'));
+    const dow = {0:'sun',1:'mon',2:'tue',3:'wed',4:'thu',5:'fri',6:'sat'}
+    const getDow = x => dow[x.day()];
+</script>
+<style>
+	div .cal {
+		/* this will only affect <p> elements in this component */
+		color: burlywood;
+        display: grid;
+        grid-template-columns:
+            repeat(var(--days-width), minmax(10px, 1fr));
+            /* column-gap: 10px; */
+        align-items: stretch;
+	}
+    div .day {
+        border:2px solid;
+        margin: .4em;
+        align-self: stretch;
+        height:10em;
+        
+    }
+    .sun, .sat {
+        background-color: gainsboro;
+    }
+</style>
+<div>
+    between {start} and {end}, there are {daysCount} days. that is {weeksCount} weeks, or {monthsCount} months
+    split by {w}
+    <div class='cal'  style="--days-width: {w}">
+    {#each days as day,i }
+		<div id={i} class='day {getDow(day)}'> {day.format('MMM D')} </div>
+	{/each}
+    </div>
+</div>
